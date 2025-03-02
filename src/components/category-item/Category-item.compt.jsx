64e800d97ed20da71c509eef3c268db880dd9170
Aqil321821 <aqilrao321821@gmail.com>
2025-03-03 01:40:09 +0500

    added naviagte function in cate-item

--- src/components/category-item/Category-item.compt.jsx
+++ src/components/category-item/Category-item.compt.jsx
@@ -1,7 +1,12 @@
 import './category-item.styles.scss';
+import { useNavigate } from 'react-router-dom';
 const CategoryItem = ({ category }) => {
+  const navigate = useNavigate();
+  const handle = () => {
+    navigate(`shop/${category.title}`);
+  };
   return (
-    <div className='category-container' >
+    <div className='category-container' onClick={handle}>
       <div className='background-image' style={{ backgroundImage: `url(${category.imageUrl})` }} />
       <div className='category-body-container'>
         <h2>{category.title}</h2>
